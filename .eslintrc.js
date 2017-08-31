39c7b821baa9fa36d88739faa25a3936ce6fb454
刘金建 <liujinjian@9fbank.cc>
2017-08-31 17:44:54 +0800

    提交隐藏文件

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "extends": ["eslint:recommended", "plugin:node/recommended"],
+  "plugins": [
+    "node"
+  ],
+  "globals": {
+  },
+  // add your custom rules here
+  "rules": {
+    "no-new":0,
+    "no-unused-vars": ["error", {"args": "none" }],
+    "eqeqeq":0,//设置为0，代表 == 也可以不必要非得 ===
+    "quotes":0,
+    "no-extra-boolean-cast":0,
+    "space-infix-ops":0,
+    "handle-callback-err": 0,
+    "new-cap": 0,//函数名首行大写必须使用new方式调用，首行小写必须用不带new方式调用
+    // allow paren-less arrow functions
+    "arrow-parens": 0,
+    "properties": 0,
+    "camelcase":0,//不必要非得用骆驼拼接法
+    // allow async-await
+    "enerator-star-spacing": 0,
+    // "react/jsx-uses-react": "error",
+    // "react/jsx-uses-vars": "error",
+    // allow debugger during development
+    "no-debugger": 0,
+    "node/exports-style": ["error", "module.exports"]
+  }
+}
